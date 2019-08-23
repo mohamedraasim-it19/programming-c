1010338f3baeadf1e41fe394fb77c45770980b8c
mohamedraasim-it19 <mohamedraasim.it19@bitsathy.ac.in>
2019-08-23 09:51:47 +0530

    first commit

---
+++ multiple.php
@@ -0,0 +1,24 @@
+
+<?php
+
+$servername = "121.200.55.60:3307";
+$username = "2019UIT1103";
+$password = "2019UIT1103";
+$database = "2019UIT1103";
+$conn = mysqli_connect($servername, $username, $password,$database);
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully <br />";
+$trunc="truncate table multiple_table";
+mysqli_query($conn,$trunc);
+echo "table truncated <br />";
+for($i=1;$i<=30;$i++)
+{
+$result=$i*5;
+$sql="insert into multiple_table(multiplicant,multiplier,result)values('$i',5,'$result')";
+$res=mysqli_query($conn,$sql);
+echo " $res <br />";
+mysqli_close($conn);
+}
+?>
